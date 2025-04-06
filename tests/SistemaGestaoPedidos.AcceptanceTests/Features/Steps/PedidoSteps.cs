3e4f23670ab1fefc4d10b50e8e79e3189f209222
LuizGermano <mgermano578@gmail.com>
2025-04-05 22:02:46 -0300

    Update PedidoSteps.cs

--- tests/SistemaGestaoPedidos.AcceptanceTests/Features/Steps/PedidoSteps.cs
+++ tests/SistemaGestaoPedidos.AcceptanceTests/Features/Steps/PedidoSteps.cs
@@ -15,14 +15,14 @@ namespace SistemaGestaoPedidos.AcceptanceTests.Features.Steps
 		private decimal _valorTotalCalculado;
 
 		[Given(@"que um cliente chamado ""(.*)"" deseja fazer um pedido")]
-		public void DadoQueUmClienteChamadoDesejaFazerUmPedido(string nome)
+		public void FazerUmPedido(string nome)
 		{
 			_cliente = new Cliente(nome, "email@teste.com");
 			_pedido = new Pedido(_cliente);
 		}
 
 		[When(@"ele adiciona os seguintes itens ao pedido:")]
-		public void QuandoEleAdicionaOsSeguintesItensAoPedido(Table table)
+		public void ItensAoPedido(Table table)
 		{
 			foreach (var row in table.Rows)
 			{
@@ -36,20 +36,20 @@ namespace SistemaGestaoPedidos.AcceptanceTests.Features.Steps
 		}
 
 		[When(@"ele confirma o pedido")]
-		public void QuandoEleConfirmaOPedido()
+		public void ConfirmaOPedido()
 		{
 			_pedido.ConfirmarPedido();
 		}
 
 		[Then(@"o pedido deve ter o status ""(.*)""")]
-		public void EntaoOPedidoDeveTerOStatus(string statusEsperado)
+		public void Status(string statusEsperado)
 		{
 			var statusReal = _pedido.Status.ToString();
 			statusReal.Should().Be(statusEsperado);
 		}
 
 		[Then(@"o valor total do pedido deve ser (.*)")]
-		public void EntaoOValorTotalDoPedidoDeveSer(decimal valorEsperado)
+		public void ValorTotalPedido(decimal valorEsperado)
 		{
 			_valorTotalCalculado = _pedido.ValorTotal;
 			_valorTotalCalculado.Should().Be(valorEsperado);
